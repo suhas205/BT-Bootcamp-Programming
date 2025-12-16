dc37a77f09c1b6c6517a26a42734913f970c0a2f
Suhas T S <suhastms2004@gmail.com>
2025-12-16 08:10:48 +0530

    Update solution.py

--- Q08/solution.py
+++ Q08/solution.py
@@ -7,6 +7,6 @@ elif b>a and b>c:
     print("b is largest",b)
 else:   
     print("c is largest",c)
-
+#other way of doing this code
 print("we can do it with using max function too")
 print("largest is", max(a,b,c))
