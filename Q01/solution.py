30e3b676b2647335aec670878c689db52b6c1880
Suhas T S <suhastms2004@gmail.com>
2025-12-16 08:19:18 +0530

    solution.py

--- Q01/solution.py
+++ Q01/solution.py
@@ -1,4 +1,8 @@
-a=int(input("Enter first number: "))
-b=int(input("Enter second number: "))
-print("sum is",a+b)
-print("Average is", (a+b)/2)
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+sum_val = a + b
+avg = sum_val / 2
+
+print("Sum =", sum_val)
+print("Average =", avg)
